90903fad1609f002505f63f12b301653f53ed82e
dbrown612345-lang <dbrown612345@gmail.com>
2026-04-24 14:56:15 -0400

    Update

--- browser.py
+++ browser.py
@@ -32,6 +32,21 @@ from PyQt6.QtMultimediaWidgets import QVideoWidget
 
 from openai import OpenAI
 
+print(
+  _____              _      _   ____                                    
+ |  __ \            (_)    | | |  _ \                                   
+ | |  | | __ _ _ __  _  ___| | | |_) |                                  
+ | |  | |/ _` | '_ \| |/ _ \ | |  _ <                                   
+ | |__| | (_| | | | | |  __/ | | |_) |                                  
+ |_____/ \__,_|_| |_|_|\___|_| |____/                         __   ___  
+ |  __ \             |  _ \                                  /_ | / _ \ 
+ | |  | | ___  _ __  | |_) |_ __ _____      _____  ___ _ __   | || | | |
+ | |  | |/ _ \| '__| |  _ <| '__/ _ \ \ /\ / / __|/ _ \ '__|  | || | | |
+ | |__| | (_) | |    | |_) | | | (_) \ V  V /\__ \  __/ |     | || |_| |
+ |_____/ \___/|_|    |____/|_|  \___/ \_/\_/ |___/\___|_|     |_(_)___/ 
+                                                                        
+ )
+
 #If you are unlucky enough to debug this code im sorry -Daniel
 #if you are here to edit and you made a change leave your name
 #1.Daniel
